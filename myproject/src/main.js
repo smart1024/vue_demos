bd8294111478297f3c543bb12dc4268ec670ec0e
liling <1059232202@qq.com>
2020-07-23 22:49:22 +0800

    vue-cli创建项目

---
+++ myproject/src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+//入口文件
+
+//引入Vue构造函数
+import Vue from 'vue'
+
+//引入根组件
+import App from './App'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
