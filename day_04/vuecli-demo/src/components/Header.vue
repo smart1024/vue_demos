c1799eafa6f5318740f122b9b1ba2723443fd59e
liling <1059232202@qq.com>
2020-07-25 16:59:22 +0800

    声明周期函数

--- day_04/vuecli-demo/src/components/Header.vue
+++ day_04/vuecli-demo/src/components/Header.vue
@@ -24,6 +24,31 @@ export default {
             this.$emit("changeMsg","小李子通过$emit修改了msg值");
         }
     },
+    // 声明周期函数
+    beforeCreate() {
+        console.log("beforeCreate----vue实例创建之前");
+    },
+    created() {
+        console.log("created----vue实例创建成功");
+    },
+    beforeMount() {
+        console.log("beforeMount-----vue实例挂载之前,页面是空白，这时render函数被调用");
+    },
+    mounted() {
+        console.log("mounted----这时你才可以看到页面，也可以访问Dom了");
+    },
+    beforeUpdate() {
+        console.log("beforeUpdate--------数据发生变化就会触发");
+    },
+    updated() {
+        console.log("updated----------数据更新完毕");
+    },
+    beforeDestroy() {
+        console.log("beforeDestroy-----当前实例被销毁之前，释放一些资源定时器、第三方Dom");
+    },
+    destroyed() {
+        console.log("destroyed--------------当前实例被销毁");
+    },
 }
 </script>
 
