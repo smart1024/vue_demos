8b60ebea7a068ddc476f3c3e4c7c7914e5d15f07
liling <1059232202@qq.com>
2020-07-25 20:55:42 +0800

    端口/https/跨域等配置

--- day_04/vue-demo2/vue.config.js
+++ day_04/vue-demo2/vue.config.js
@@ -7,4 +7,14 @@ module.exports={ //Invalid options in vue.config.js: "baseUrl" is not allowed
  */
     publicPath:'/', //根路径
     // outputDir:'dist' //build输出路径，不指定默认dist目录
+    lintOnSave:true,//是否开启eslint保存检测
+    devServer:{
+        open:true, //允许自动打开浏览器
+        host:'localhost', //默认主机localhost
+        port:8080, //端口号，默认8080
+        https:true, //是否开启https，默认是http
+        proxy:{ //跨域配置
+
+        }
+    }
 }
